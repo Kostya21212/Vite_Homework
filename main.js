79126c357a481699b2f0078289b5e2fa2781b6aa
Onischenko Kostyantyn <Onischenko411@gmail.com>
2024-02-01 03:06:50 +0200

    add

---
+++ main.js
@@ -0,0 +1,12 @@
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import { numbers,numbers2,numbers3,numbers4,number5 } from './homework'
+
+
+
+document.querySelector('#app').innerHTML = /*html*/`
+  <div class = 'justify-center'>
+    <h1 classname =' text-7xl font-bold underline text-stone-700 justify-center'>Hello world</h1>
+  </div>
+`
